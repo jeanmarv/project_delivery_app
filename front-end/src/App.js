d43c02d4ad1261141490903ea841acfdaaa03d95
Filipe <filipef.paulo@gmail.com>
2022-07-12 19:34:30 -0300

    fix(routes): Changing root / to redirect for /login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -8,7 +7,7 @@ import Register from './pages/Register';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Login /> } />
+      <Route path="/" element={ <Navigate to="/login" /> } replace />
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
     </Routes>
