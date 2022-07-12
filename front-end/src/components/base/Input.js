7f64932d7821962a8f78842307862133ef709f4a
Filipe <filipef.paulo@gmail.com>
2022-07-12 19:36:10 -0300

    feat(base-components): Creating the base components with styled-components

---
+++ front-end/src/components/base/Input.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  background-color: inherit;
+  border: 1px solid black;
+  color: inherit;
+  padding: 10px;
+  border-radius: 10px;
+  width: 100%;
+`;
+
+export default Input;
