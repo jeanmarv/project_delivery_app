5f7d84208014c98ca235aff799a3fe57bd4664e6
Filipe <filipef.paulo@gmail.com>
2022-07-13 20:08:57 -0300

    feat(new-route): Add new route /seller/orders

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import SellerOrders from './pages/SellerOrders';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={ <Navigate to="/login" /> } replace />
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
+      <Route path="/seller/orders" element={ <SellerOrders /> } />
     </Routes>
   );
 }
