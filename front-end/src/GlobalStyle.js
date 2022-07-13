5b3071bac735cd905adc4f8574d7c590f82a306d
Filipe <filipef.paulo@gmail.com>
2022-07-13 20:15:42 -0300

    improvement(new-colors): Add new colors in GlobalStyle

--- front-end/src/GlobalStyle.js
+++ front-end/src/GlobalStyle.js
@@ -7,18 +7,25 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
+    color: var(--color-dark);
   }
 
-  p, h1, h2, h3, h4, h5, h6, span {
+  p, h1, h2, h3, h4, h5, h6, span, div {
     font-size: 16px;
     font-family: 'Roboto', sans-serif;
+    color: var(--color-dark);
   }
 
   :root {
     --color-main-green: #036B52;
     --color-light-green: #2fc18c;
 
-    --color-white: white;
+    --color-blue: #056cf9;
+    --color-purple: #421981;
+
+    --color-white: #f2fffc;
+
+    --color-dark: #001813;
 
     --color-light-gray: #eaf1ef;
     --color-medium-gray: #b1c2be;
