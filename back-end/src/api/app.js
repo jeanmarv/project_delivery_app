0003025bd92f6daa3ee07c1aed1ddb9564599b4a
finudofinu <marcelo.cunha1@outlook.com>
2022-07-14 08:26:44 -0300

    chore: api set routes

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
+const loginRoutes = require('./routers/login.routes');
+const registerRoutes = require('./routers/register.routes');
 
 const app = express();
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.use(express.json());
+app.use(cors());
+
+app.use('/', loginRoutes);
+app.use('/', registerRoutes);
 
 module.exports = app;
