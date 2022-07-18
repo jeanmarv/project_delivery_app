e4a4b241031c65a8330ec867409c776d1d218aea
Filipe <filipef.paulo@gmail.com>
2022-07-18 18:12:16 -0300

    improvement: Adding validation in the body of login request

--- back-end/src/api/services/login.service.js
+++ back-end/src/api/services/login.service.js
@@ -2,6 +2,8 @@ const md5 = require('md5');
 const { User } = require('../../database/models');
 
 const login = async ({ email, password }) => {
+    if (!email || !password) return null;
+
     const hashPassword = md5(password);
     const userLogin = await User.findOne({ where: { email, password: hashPassword } });
 
