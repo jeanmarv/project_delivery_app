4bedc5aacc490e620eb02b1210c56bf77268770d
Douglas <douglasfmr1@gmail.com>
2022-07-21 16:47:37 -0300

    feat(CRUD Sales): sales backEnd

---
+++ back-end/src/api/services/sales.service.js
@@ -0,0 +1,73 @@
+const { Sales, Products, SalesProducts, User } = require("../../database/models");
+
+const createSales = async (body) => {
+  const {
+    totalPrice,
+    deliveryAddress,
+    status,
+    deliveryNumber,
+    userId,
+    sellerId,
+    productsSale,
+  } = body;
+
+  const sale = await Sales.create({
+    totalPrice,
+    deliveryAddress,
+    deliveryNumber,
+    status,
+    userId,
+    sellerId,
+    productsSale,
+  });
+
+  await Promise.all(
+    productsSale.map(async (product) => {
+      const { id, quantity } = product;
+      const productSale = await SalesProducts.create({
+        sale_id: sale.dataValues.id,
+        product_id: id,
+        quantity,
+      });
+      return productSale;
+    })
+  );
+  return sale.dataValues;
+};
+
+const getAllSales = async (email) => {
+  const user = await User.findOne({ where: { email } });
+  const sales = await Sales.findAll({
+    where: { sellerId: user.id },
+    });
+  return sales;
+}
+
+const getSalesById = async (id) => {
+  const sale = await Sales.findOne({
+    where: { id },
+    include: [
+      {
+        model: Products,
+        as: "Products",
+      },
+    ],
+  });
+  return sale;
+}
+
+const updateStatusSales = async (id, status) => {
+  const sale = await Sales.update(
+    { status },
+    { where: { id } }
+  );
+  return getSalesById(id);
+}
+
+module.exports = {
+    createSales,
+    getAllSales,
+    getSalesById,
+    updateStatusSales,
+};
+
