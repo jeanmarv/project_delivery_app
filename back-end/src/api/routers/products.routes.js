32766ced40e1f6c48e2681209714c8933e65e589
Victor Emmanuel <victorpassos2012@gmail.com>
2022-07-22 16:12:34 -0300

    Corrige o lint

--- back-end/src/api/routers/products.routes.js
+++ back-end/src/api/routers/products.routes.js
@@ -2,10 +2,9 @@ const { Router } = require('express');
 const { getProducts, getProductById } = require('../controllers/product.controller');
 const validateToken = require('../middlewares/token.auth');
 
-
 const router = Router();
 
-router.get('/products',validateToken , getProducts);
-router.get('/products/:id',validateToken , getProductById);
+router.get('/products', validateToken, getProducts);
+router.get('/products/:id', validateToken, getProductById);
 
 module.exports = router;
