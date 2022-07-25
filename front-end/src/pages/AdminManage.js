ed80c0af55754262ea388965a58e67389b520eb4
renansouza95 <s.renan@outlook.com>
2022-07-25 18:26:25 -0300

    fix: renderizacao condicional de erro

--- front-end/src/pages/AdminManage.js
+++ front-end/src/pages/AdminManage.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
 import AdminForm from '../components/AdminForm';
 import AdminTable from '../components/AdminTable';
 import Header from '../components/Header';
+import GlobalContext from '../context/GlobalContext';
 
 export default function AdminManage() {
+  const { user, error } = useContext(GlobalContext);
+
+  if (user.role !== 'administrator') {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
       <Header />
@@ -14,6 +22,14 @@ export default function AdminManage() {
       <br />
       <h2>Cadastrar novo usuário</h2>
       <AdminForm />
+      {error && (
+        <p
+          className="errorMessage"
+          data-testid="admin_manage__element-invalid-register"
+        >
+          {error}
+        </p>
+      )}
       <AdminTable />
     </div>
   );
