a18b5ef6154fe4b6184478be1b1ff0b0f89a65b3
renansouza95 <s.renan@outlook.com>
2022-07-26 20:31:30 -0300

    feat: renderiza os cards na pagina do vendedor

--- front-end/src/pages/SellerOrders.js
+++ front-end/src/pages/SellerOrders.js
@@ -1,6 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { getSellerOrders } from '../api/requests';
+import SellerCard from '../components/SellerCard';
 import ContainerCenter from '../components/base/ContainerCenter';
 import Header from '../components/Header';
 import GlobalContext from '../context/GlobalContext';
@@ -12,17 +14,54 @@ const SellerOrdersContainer = styled(ContainerCenter)`
 `;
 
 export default function SellerOrders() {
-  const { user, resetUser } = useContext(GlobalContext);
+  const { user, resetUser, sellerOrders, setSellerOrders } = useContext(GlobalContext);
+
+  useEffect(() => {
+    console.log(user);
+    async function fetchOrders() {
+      const request = await getSellerOrders(user.email);
+      if (request.error) return;
+
+      setSellerOrders(request);
+    }
+    fetchOrders();
+  }, [setSellerOrders]);
 
   if (user.role !== 'seller') {
     resetUser();
     return <Navigate to="/" />;
   }
 
+  // const sellerOrders = [
+  //   {
+  //     id: 1,
+  //     totalPrice: 135.47,
+  //     deliveryAddress: 'Rua Dois',
+  //     deliveryNumber: 70,
+  //     status: 'Pendente',
+  //     saleDate: '26/07/2022'
+  //   },
+  //   {
+  //     id: 2,
+  //     totalPrice: 535,
+  //     deliveryAddress: 'Rua Dois',
+  //     deliveryNumber: 70,
+  //     status: 'Em transito',
+  //     saleDate: '06/07/2022'
+  //   },
+  // ];
+
   return (
     <SellerOrdersContainer>
       <Header />
-      SellerOrders
+      <div>
+        {sellerOrders && sellerOrders.map((order) => (
+          <SellerCard
+            key={ order.id }
+            order={ order }
+          />
+        ))}
+      </div>
     </SellerOrdersContainer>
   );
 }
