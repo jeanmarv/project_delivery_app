4dd6dc80b715f32028195dc091cf081d780edc33
Victor Emmanuel <victorpassos2012@gmail.com>
2022-07-28 14:56:57 -0300

    Cria a rota customer order id

--- back-end/src/api/routers/sales.routes.js
+++ back-end/src/api/routers/sales.routes.js
@@ -6,8 +6,9 @@ const router = Router();
 
 router.post('/seller/orders', validateToken, salesController.createSalesController);
 router.get('/seller/orders', validateToken, salesController.getAllSellerSalesController);
-router.get('/custumer/orders', validateToken, salesController.getAllCustumerSalesController);
+router.get('/customer/orders', validateToken, salesController.getAllCustumerSalesController);
 router.get('/seller/orders/:id', validateToken, salesController.getSalesByIdController);
+router.get('/customer/orders/:id', validateToken, salesController.getSalesByIdController);
 router.put('/seller/orders/:id', validateToken, salesController.updateStatusSalesController);
 
 module.exports = router;
