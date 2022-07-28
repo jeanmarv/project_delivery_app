7b7b2e8dc744a4921024f66393168060d7b5fe44
renansouza95 <s.renan@outlook.com>
2022-07-28 18:13:57 -0300

    fix: lint de helpers

--- front-end/src/components/CustomerCard.js
+++ front-end/src/components/CustomerCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { formatDate } from '../helpers/formatDate';
+import formatDate from '../helpers/formatDate';
 
 export default function CustomerCard({ order }) {
   const {
