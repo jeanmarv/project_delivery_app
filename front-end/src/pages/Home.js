28f406aee7fb5f3f7ade28d0bb052f2d07b3bc9b
renansouza95 <s.renan@outlook.com>
2022-07-28 17:43:14 -0300

    implementa logica de ler o localStorage na Home

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
+import GlobalContext from '../context/GlobalContext';
 
 const avaibleRoles = ['administrator', 'seller', 'customer'];
 
 export default function Home() {
+  const { setUser } = useContext(GlobalContext);
+
   if (!localStorage.user) return <Navigate to="/login" />;
 
   const user = JSON.parse(localStorage.user);
+  setUser(user);
+
   if (user.role === avaibleRoles[0]) return <Navigate to="/admin/manage" />;
   if (user.role === avaibleRoles[1]) return <Navigate to="/seller/orders" />;
   if (user.role === avaibleRoles[2]) return <Navigate to="/customer/products" />;
